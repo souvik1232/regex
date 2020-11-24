97bd6d6960d9d2b814e88e7e872c24cd4c8464b4
souvik1232 <souvik199831@gmail.com>
2020-11-24 13:07:09 +0530

    Added:Pattern for Lastname

--- regex.sh
+++ regex.sh
@@ -8,3 +8,13 @@ do
 	read -p "Enter a valid first name" fname
 	echo "Invalid input"
 done
+
+
+read -p "Enter a valid last name" lname
+
+input2="^[A-Z]{1}[a-z]{2,}$"
+while(( $lname =~ $input2 ))
+do
+        read -p "Enter a valid first name" lname
+        echo "Invalid input"
+done
